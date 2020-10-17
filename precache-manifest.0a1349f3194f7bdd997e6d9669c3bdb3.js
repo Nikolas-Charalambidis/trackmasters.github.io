2a32adc3fac8e9f562268511a36bd229cc7fed83
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-17 21:00:17 +0000

    Deploy Nikolas-Charalambidis/trackmasters.github.io to github.com/Nikolas-Charalambidis/trackmasters.github.io.git:master

---
+++ precache-manifest.0a1349f3194f7bdd997e6d9669c3bdb3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a12042ea5322811a787735027d2ce2df",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d2364955cc648fdc5803",
+    "url": "./static/css/2.694d878d.chunk.css"
+  },
+  {
+    "revision": "d2de6f13affe784bef24",
+    "url": "./static/css/main.e672ce61.chunk.css"
+  },
+  {
+    "revision": "d2364955cc648fdc5803",
+    "url": "./static/js/2.60ac56ab.chunk.js"
+  },
+  {
+    "revision": "d2de6f13affe784bef24",
+    "url": "./static/js/main.b055be39.chunk.js"
+  },
+  {
+    "revision": "9fbdc6fb9046eb146f3a",
+    "url": "./static/js/runtime-main.46091448.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "./static/media/introduction.d41d8cd9.txt"
+  },
+  {
+    "revision": "9411cda8a7a05fb4d7bd6a81e112115b",
+    "url": "./static/media/logo.9411cda8.jpg"
+  }
+]);
