5c9c9ccae7cf4521da3b8758b81a1ce903c62607
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-29 23:17:15 +0000

    Deploy Nikolas-Charalambidis/trackmasters.github.io to github.com/Nikolas-Charalambidis/trackmasters.github.io.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74280475292f4b75a7c4089380cea638.js"
+  "/trackmasters.github.io/precache-manifest.10a49bd8bf515692213d63bed0e2a9ae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trackmasters.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
