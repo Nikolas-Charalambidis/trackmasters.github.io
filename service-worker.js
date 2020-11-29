50b64e9ef5b7a62fb4e7b913594d26bb2f0b6ea7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-29 22:31:57 +0000

    Deploy Nikolas-Charalambidis/trackmasters.github.io to github.com/Nikolas-Charalambidis/trackmasters.github.io.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1abdb7a4a273e1935f768fe0cead93de.js"
+  "./precache-manifest.469d51c22d987e18857a77dda45625cf.js"
 );
 
 self.addEventListener('message', (event) => {
